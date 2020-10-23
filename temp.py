d8477e4b3b931bfd7e5fc5019265f9ca73db65e9
Rachaelllwong <73184272+Rachaelllwong@users.noreply.github.com>
2020-10-24 01:49:30 +0800

    Update temp.py
    Added path function and plotting the shortest path

--- temp.py
+++ temp.py
@@ -2,63 +2,109 @@ import matplotlib.pyplot as plt
 
 maze = []
 walls = []
-Coordinates = []
-new_x=[]
-new_y=[]
+Start =[]
+end = [47,1]
 
 with open(r"C:\Users\racha\OneDrive\Desktop\maze.txt", 'r') as f:
     line = f.readlines() #reading the lines in the file
     
     for element in line:
         if element == 'True\n': 
-            walls.append(0) 
+            walls.append(1) 
         elif element == "False\n":
-            walls.append(1)
+            walls.append(0)
         if len(walls) == 51:
             maze.append(walls)
             walls = []
-    
+
 #starting point
 def Startpoint_input():
     for i in range(0,2):
         number = float(input("Enter your coordinates: "))
         if number<50 and number>0:       
-            Coordinates.append(number)
+            Start.append(int(number))
         else:
-            print ('Please re-enter your coordinates.')
-            Coordinates.clear()
+            print ('Out of bounds, please re-enter your coordinates.')
+            Start.clear()
             Startpoint_input() #rerun loop function
             break
 Startpoint_input()
 
 #Checking for walls
 def Startpoint_Coords():
-    x = Coordinates[0] 
-    y = Coordinates[1]
-    print(x,y)
     #determining of value of coord
-    List1 = maze[int(y)]
-    List2 = List1[int(x)]
+    List1 = maze[Start[0]]
+    List2 = List1[Start[1]]
     if List2 == 1:
-        new_x.append(x+0.5)
-        new_y.append(y+0.5)
-    else:
         print("Coordinate is a wall, please re-enter your coordinate")
-        Coordinates.clear()
+        Start.clear()
         Startpoint_input()
         Startpoint_Coords()
 Startpoint_Coords()
 
+#Creating a 0 matrix
+p = []
+for i in range(len(maze)):
+    p.append([])
+    for j in range(len(maze[i])):
+        p[-1].append(0)
+        
+p[Start[1]][Start[0]] = 1
+
+def steps(k):
+    for y in range(len(p)):
+        for x in range(len(p[y])):
+            if p[y][x] == k:
+                if y>0 and maze[y-1][x] == 0 and p[y-1][x] == 0:
+                    p[y-1][x] = k+1
+                if x>0 and maze[y][x-1] == 0 and p[y][x-1] == 0:
+                    p[y][x-1] = k+1
+                if y<(len(p)-1) and maze[y+1][x] == 0 and p[y+1][x] == 0: 
+                    p[y+1][x] = k+1
+                if x<len(p[y])-1 and maze[y][x+1] == 0 and p[y][x+1] == 0:
+                    p[y][x+1] = k+1
+
+k=1
+while p[end[1]][end[0]] == 0:
+    steps(k)
+    k += 1
+
+path_taken = []
+def path():
+    y,x = end[1],end[0]
+    k = p[y][x]
+
+    while k > 1:
+        if y > 0 and p[y-1][x] == k-1:
+            x,y = x,y-1
+            path_taken.append((x,y))
+            k-=1
+        elif x > 0 and p[y][x-1] == k-1:
+            x,y = x-1,y
+            path_taken.append((x,y))
+            k-=1
+        elif y<len(maze)-1 and p[y+1][x] == k-1:
+            x,y = x,y+1
+            path_taken.append((x,y))
+            k-=1
+        elif x < len(maze[y]) - 1 and p[y][x+1] == k-1:
+            x,y = x+1,y
+            path_taken.append((x,y))
+            k -= 1
+path()
+
+#add path taken into maze[]
+for a in range(len(path_taken)):
+    maze[path_taken[a][1]][path_taken[a][0]]=2
+    
 #visualising maze
-plt.style.use('grayscale')
 plt.pcolormesh(maze)
 plt.axes().set_aspect('equal') #set the x and y axes to the same scale
-#plt.xticks([]) # remove the tick marks by setting to an empty list
-#plt.yticks([]) # remove the tick marks by setting to an empty list
+plt.xticks([]) # remove the tick marks by setting to an empty list
+plt.yticks([]) # remove the tick marks by setting to an empty list
 
 #plotting points
-endpoint = plt.plot(47.5,1.5,'ro')
-startpoint = plt.plot(new_x[0],new_y[0],'go')
+endpoint = plt.plot(end[0]+0.5,end[1]+0.5,'ro')
+startpoint = plt.plot(Start[0]+0.5,Start[1]+0.5,'go')
 
 plt.show()
-
